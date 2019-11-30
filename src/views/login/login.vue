72f9a6cacecc2c91a8b3aaa658b746199e29d9f3
dsdvvv <2473378640@qq.com>
2019-11-30 20:58:20 +0800

    基本登录

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -6,30 +6,54 @@
     <!-- 登录表单 -->
     <van-cell-group>
       <!-- 手机号 -->
-      <van-field placeholder="请输入手机号" left-icon="contact" />
+      <van-field v-model="user.mobile" placeholder="请输入手机号" left-icon="contact" />
 
       <!-- 密码 -->
-      <van-field type="password" placeholder="请输入密码" left-icon="bag-o" />
+      <van-field v-model="user.code" type="password" placeholder="请输入验证码" left-icon="bag-o" />
     </van-cell-group>
 
     <!-- 登录按钮 -->
     <div class="login-box">
-      <van-button color="linear-gradient(to right, #4bb0ff, #6149f6)">登录</van-button>
+      <van-button
+      color="linear-gradient(to right, #4bb0ff, #6149f6)"
+      @click="onLogin"
+      >登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request.js'
+// import {login} from '@/api/user.js'
+
 export default {
   name: 'LoginPage', // 组件名称:便于调试
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      }
+    }
   },
   computed: {},
   created () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      try {
+        const res = await request({
+          method: 'POST',
+          url: '/app/v1_0/authorizations',
+          data: this.user
+        })
+        console.log('登录成功', res)
+      } catch (error) {
+        console.log('登录失败', error)
+      }
+    }
+  }
 }
 </script>
 
