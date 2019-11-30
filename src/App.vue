7f5a29e173e3749ed2e0121aaa5a3340745f1635
dsdvvv <2473378640@qq.com>
2019-11-30 17:45:06 +0800

    导入vant-测试

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <van-button color="linear-gradient(to right, #4bb0ff, #6149f6)">渐变色按钮</van-button>
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  Button,
+  Switch
+} from 'vant'
+
+Vue
+  .use(Button)
+  .use(Switch)
 
 Vue.config.productionTip = false
 
